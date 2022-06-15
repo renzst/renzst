daac36076c4018aa53b27edd7a1edd0f913dfc4f
Renz Torres <wr3ns@protonmail.com>
2022-06-15 10:10:50 -0400

    Commented out and moved resources for future

--- OdinProject/08_WebpackTutorial/src/index.js
+++ OdinProject/08_WebpackTutorial/src/index.js
@@ -1,18 +1,18 @@
 import _ from 'lodash';
 import './style.css';
-import Hello from './hello.jpg';
+/* import Hello from './hello.jpg';
 import Data from './data.xml';
 import Notes from './data.csv';
 import toml from './data.toml';
 import yaml from './data.yaml';
-import json from './data.json5';
+import json from './data.json5'; */
 
-console.log(toml.title); // output `TOML Example`
+/* console.log(toml.title); // output `TOML Example`
 console.log(toml.owner.name); // output `Tom Preston-Werner`
 console.log(yaml.title); // output `YAML Example`
 console.log(yaml.owner.name); // output `Tom Preston-Werner`
 console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
+console.log(json.owner.name); // output `Tom Preston-Werner` */
 // import myName from './myName';
 
 function component() {
@@ -20,7 +20,7 @@ function component() {
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+/*   element.classList.add('hello');
 
   const myHello = new Image();
   myHello.src = Hello;
@@ -31,7 +31,7 @@ function component() {
   console.log(Notes);
   // use your function!
   // element.textContent = myName('Cody');
-
+ */
 
   return element;
 }
--- OdinProject/08_WebpackTutorial/webpack.config.js
+++ OdinProject/08_WebpackTutorial/webpack.config.js
@@ -1,14 +1,15 @@
 const path = require('path');
-const toml = require('toml');
+/* const toml = require('toml');
 const yaml = require('yamljs');
-const json5 = require('json5');
+const json5 = require('json5'); */
+
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  module: {
+/*   module: {
     rules: [
       {
         test: /\.css$/i,
@@ -52,5 +53,5 @@ module.exports = {
         },
       },
     ],
-  },
+  }, */
 };
