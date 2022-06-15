8a06ce838b858a088644d52701bb2d10ae458495
Renz Torres <wr3ns@protonmail.com>
2022-06-15 15:21:08 -0400

    Next section of OdinProject

--- OdinProject/08_WebpackTutorial/src/index.js
+++ OdinProject/08_WebpackTutorial/src/index.js
@@ -1,9 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-/* import Hello from './hello.jpg';
+// import './style.css'; // requires a rule in webpack.config.js, style-loader and css-loader installed
+import printMe from './print.js';
+/* import Hello from './hello.jpg'; // requires a rule in config of type asset/resource
 import Data from './data.xml';
 import Notes from './data.csv';
-import toml from './data.toml';
+import toml from './data.toml'; // requires rules, package to be loaded (csv-loader) and use declared
 import yaml from './data.yaml';
 import json from './data.json5'; */
 
@@ -17,9 +18,13 @@ console.log(json.owner.name); // output `Tom Preston-Werner` */
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  btn.innerHTML = 'Click me and check the console';
+  btn.addEventListener("click", printMe());
+  element.appendChild(btn);
 /*   element.classList.add('hello');
 
   const myHello = new Image();
--- OdinProject/08_WebpackTutorial/webpack.config.js
+++ OdinProject/08_WebpackTutorial/webpack.config.js
@@ -1,13 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 /* const toml = require('toml');
 const yaml = require('yamljs');
 const json5 = require('json5'); */
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 /*   module: {
     rules: [
